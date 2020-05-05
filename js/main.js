64aea20a6cbaa1b7ad73fce8d81996ff77df7b4f
Hill1985 <62746185+Hill1985@users.noreply.github.com>
2020-05-05 17:10:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const cartButton = document.querySelector('#cart-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+cartButton.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
+}
+
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const loginForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('Delivery');
+
+//console.dir(modalAuth);
+
+function toogleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+function validateForm() {
+  let validForm = document.forms['valForm']['valName'].value;
+  if(validForm == '') {
+    alert('Field is Empty!!');
+    return false;
+  }
+}
+function authorized() {
+
+  function logOut() {
+    login = null;
+    localStorage.removeItem('Delivery');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+
+  //console.log('Auth');
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut);
+
+}
+
+
+function notAuthorized() {
+  //console.log('Not Auth');
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    localStorage.setItem('Delivery', login);
+
+    toogleModalAuth();
+    buttonAuth.removeEventListener('click', toogleModalAuth);
+    closeAuth.removeEventListener('click', toogleModalAuth);
+    loginForm.removeEventListener('submit', logIn);
+    loginForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener('click', toogleModalAuth);
+  closeAuth.addEventListener('click', toogleModalAuth);
+  loginForm.addEventListener('submit', logIn);
+}
+
+function checkAuth() {
+  if(login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
